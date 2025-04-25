8b57d8d0c8054b1bc2ddad455a99463c69332244
Matthew Harris <rtpharry@hotmail.com>
2025-04-25 10:30:48 +0100

    feat(home): move toolbar into footer

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import {
+  IonButton,
+  IonButtons,
   IonContent,
+  IonFooter,
   IonHeader,
+  IonIcon,
   IonTitle,
   IonToolbar,
 } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import {
+  addCircleOutline,
+  clipboardOutline,
+  copyOutline,
+  refreshOutline,
+} from 'ionicons/icons';
 import { KeyboardShortcutsComponent } from './ui/keyboard-shortcuts/keyboard-shortcuts.component';
 import { TaskListComponent } from './ui/task-list/task-list.component';
 
@@ -12,11 +23,16 @@ import { TaskListComponent } from './ui/task-list/task-list.component';
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  standalone: true,
   imports: [
+    IonButton,
+    IonButtons,
+    IonIcon,
     IonHeader,
     IonToolbar,
     IonTitle,
     IonContent,
+    IonFooter,
     TaskListComponent,
     KeyboardShortcutsComponent,
   ],
@@ -24,7 +40,14 @@ import { TaskListComponent } from './ui/task-list/task-list.component';
 export class HomePage implements OnInit {
   initialCombinedList: string = '';
 
-  constructor() {}
+  constructor() {
+    addIcons({
+      addCircleOutline,
+      copyOutline,
+      clipboardOutline,
+      refreshOutline,
+    });
+  }
 
   ngOnInit() {
     const savedList = localStorage.getItem('taskList');
